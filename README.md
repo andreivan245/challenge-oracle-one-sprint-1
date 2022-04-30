c9f84b23ca42ba48b371f4bc8ea7999ed0c595e9
André Castro <andreflus887@gmail.com>
2022-04-30 14:00:20 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# Challenge Oracle ONE Sprint-1
+
+
+# Resumo 
+<p>O primeiro desafio Oracle ONE consiste em uma aplicação que criptografa/descriptografa textos.<br>
+
+<br>As chaves de criptografia usadas são as seguintes:</p>
+
+<ul>
+  <li><strong>A letra "e" é convertida para "enter"</strong></li>
+  <li><strong>A letra "i" é convertida para "imes"</strong></li>
+  <li><strong>A letra "a" é convertida para "ai"</strong></li>
+  <li><strong>A letra "o" é convertida para "ober"</strong></li>
+  <li><strong>A letra "e" é convertida para "enter"</strong></li>
+  <li><strong>A letra "u" é convertida para "ufatr"</strong></li>
+</ul>
+
+Exemplos:<br>
+<strong>"gato" => "gaitober"<br></strong>
+<strong>"gaitober" => "gato"<br></strong>
+
+# Objetivos
+<p>Essa aplicação tem como principal objetivo o estudo e desenvolvimento de HTML5, CSS3 e JavasScript. Conceitos como responsividade também estão sendo trabalhados nessa aplicação, uma vez que não há a utilização de freamworks, sendo assim, essa é uma aplicação web responsiva toda feita com CSS3.</p>
---
+++ js/script.js
@@ -0,0 +1,48 @@
+var captar = "";
+var heightPage = document.body.scrollHeight;
+
+function criptografar() {
+
+    captar = document.getElementById('texto').value;
+    captar = captar.replace(/e/gi, "enter");
+    captar = captar.replace(/i/gi, "imes");
+    captar = captar.replace(/a/gi, "ai");
+    captar = captar.replace(/o/gi, "ober");
+    captar = captar.replace(/u/gi, "ufat");
+
+    document.getElementById("resultado").style.display = "flex";
+    document.getElementById("bt-copiar").style.display = "inline";
+    document.getElementById('resultado').value = captar;
+    document.getElementById("txt-retangulo").style.display = "none";
+    document.getElementById("homem").style.display = "none"
+    document.getElementById('texto').value = "";
+
+    window.scrollTo(0, heightPage);
+}
+
+function descriptografar() {
+
+    captar = document.getElementById('texto').value;
+
+    captar = captar.replace(/enter/g, "e");
+    captar = captar.replace(/imes/g, "i");
+    captar = captar.replace(/ai/g, "a");
+    captar = captar.replace(/ober/g, "o");
+    captar = captar.replace(/ufat/g, "u");
+
+    document.getElementById('resultado').value = captar;
+    document.getElementById('texto').value = "";
+
+    window.scrollTo(0, heightPage);
+}
+
+function copiar() {
+    let copiado = document.getElementById('resultado');
+
+    copiado.select();
+    copiado.setSelectionRange(0, 99999);
+
+    document.execCommand("copy");
+    window.scrollTo(0, 0);
+
+}
